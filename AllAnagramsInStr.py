3d679a4527b204557d5f3b47d52f33475bf8bca6
Gautam-Balachandran <gautam.balachandran@gmail.com>
2024-07-13 12:31:51 -0400

    Completed Strings 2

---
+++ AllAnagramsInStr.py
@@ -0,0 +1,53 @@
+﻿# Time Complexity : O(n), where n is the length of S
+# Space Complexity : O(1)
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        result = []
+        if not s or not p or len(s) < len(p):
+            return result
+        
+        p_map = defaultdict(int)
+        for ch in p:
+            p_map[ch] += 1
+        
+        match = 0
+        for i in range(len(s)):
+            ch = s[i]
+            if ch in p_map:
+                p_map[ch] -= 1
+                if p_map[ch] == 0:
+                    match += 1
+            
+            if i >= len(p):
+                outgoing_ch = s[i - len(p)]
+                if outgoing_ch in p_map:
+                    if p_map[outgoing_ch] == 0:
+                        match -= 1
+                    p_map[outgoing_ch] += 1
+            
+            if match == len(p_map):
+                result.append(i - len(p) + 1)
+        
+        return result
+
+# Examples
+solution = Solution()
+
+# Example 1
+s1 = "cbaebabacd"
+p1 = "abc"
+print(solution.findAnagrams(s1, p1))  # Output: [0, 6]
+
+# Example 2
+s2 = "abab"
+p2 = "ab"
+print(solution.findAnagrams(s2, p2))  # Output: [0, 1, 2]
+
+# Example 3
+s3 = "af"
+p3 = "be"
+print(solution.findAnagrams(s3, p3))  # Output: []
---
+++ ImplementStrStr.py
@@ -0,0 +1,43 @@
+﻿# Time Complexity : O(m*n)
+# Space Complexity : O(1)
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack is None or needle is None or len(haystack) == 0 or len(needle) == 0:
+            return -1
+        
+        n = len(haystack)
+        m = len(needle)
+
+        if haystack == needle:  # both strings are the same
+            return 0
+
+        for i in range(n):
+            if haystack[i] == needle[0] and i + m <= n:  # beginning character match
+                if haystack[i:i + m] == needle:
+                    return i
+
+        return -1
+
+# Examples
+solution = Solution()
+
+haystack1 = "hello"
+needle1 = "ll"
+print(solution.strStr(haystack1, needle1))  # Expected output: 2
+
+haystack2 = "aaaaa"
+needle2 = "bba"
+print(solution.strStr(haystack2, needle2))  # Expected output: -1
+
+haystack3 = ""
+needle3 = ""
+print(solution.strStr(haystack3, needle3))  # Expected output: -1
+
+haystack4 = "abc"
+needle4 = "abc"
+print(solution.strStr(haystack4, needle4))  # Expected output: 0
+
+haystack5 = "mississippi"
+needle5 = "issip"
+print(solution.strStr(haystack5, needle5))  # Expected output: 4
